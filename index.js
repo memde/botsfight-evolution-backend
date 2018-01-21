6f1e250d4e28e7125f50ec1a402ec88fe3efdf95
Maria <Mariaemde@gmail.com>
2018-01-21 11:12:20 +0100

    savedata

--- index.js
+++ index.js
@@ -9,10 +9,21 @@ app.use(cors())
 app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extented:true}))
 app.use(morgan("dev"))
+const data=[]
 
-app.get("/", (req, res)=>{
-res.json({message: "Hallo"})
+app.get("/save", (req, res)=>{
+    data.push({
+        bot:req.query.bot,
+        duration: parseFloat(req. query.duration),
+        winningTeam: parseInt(req.query.winningTeam) 
+    })
+
+    res.status(204).end()
 })
+
+app.get("/result", (req, res)=>{
+    res.json(data)
+}) 
 http.createServer(app). listen(process.env.PORT||3000,()=>{
     console.log(`server running`)
 })
